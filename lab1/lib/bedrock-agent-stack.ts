96c07946dab0691c2a7bbc3851babfeea905e902
ramiroao-dev <ramiroao@amazon.com>
2024-09-19 19:09:28 -0600

    Update bedrock-agent-stack.ts

--- lab1/lib/bedrock-agent-stack.ts
+++ lab1/lib/bedrock-agent-stack.ts
@@ -25,76 +25,7 @@ export class BedrockAgentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const accesslogBucket = new s3.Bucket(this, 'AccessLogs', {
-      enforceSSL: true,
-      versioned: true,
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
-    NagSuppressions.addResourceSuppressions(accesslogBucket, [
-      {id: 'AwsSolutions-S1', reason: 'There is no need to enable access logging for the AccessLogs bucket.'},
-    ])
-    const docBucket = new s3.Bucket(this, 'DocBucket', {
-      enforceSSL: true,
-      versioned: true,
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      serverAccessLogsBucket: accesslogBucket,
-      serverAccessLogsPrefix: 'inputsAssetsBucketLogs/',
-    });
-    const kb = new bedrock.KnowledgeBase(this, 'KB', {
-      embeddingsModel: bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
-      instruction: 'Use this knowledge base to answer questions about books. ' +
-        'It contains the full text of novels. Please quote the books to explain your answers.',
-    });
-
-    const dataSource = new bedrock.S3DataSource(this, 'DataSource', {
-      bucket: docBucket,
-      knowledgeBase: kb,
-      dataSourceName: 'books',
-      chunkingStrategy: bedrock.ChunkingStrategy.FIXED_SIZE,
-      maxTokens: 500,
-      overlapPercentage: 20,
-    });
-
-    const agent = new bedrock.Agent(this, 'Agent', {
-      foundationModel: bedrock.BedrockFoundationModel.ANTHROPIC_CLAUDE_INSTANT_V1_2,
-      instruction: 'You are a helpful and friendly agent that answers questions about literature.',
-      knowledgeBases: [kb],
-      enableUserInput: true,
-      shouldPrepareAgent:true
-    });
-
-    const actionGroupFunction = new lambda_python.PythonFunction(this, 'ActionGroupFunction', {
-      runtime: lambda.Runtime.PYTHON_3_12,
-      entry: path.join(__dirname, '../lambda/action-group'),
-      layers: [lambda.LayerVersion.fromLayerVersionArn(this, 'PowerToolsLayer', `arn:aws:lambda:${this.region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:60`)],
-      timeout:cdk.Duration.minutes(2)
-    });
-
-    const actionGroup = new AgentActionGroup(this,'MyActionGroup',{
-      actionGroupName: 'query-library',
-      description: 'Use these functions to get information about the books in the library.',
-      actionGroupExecutor: {
-        lambda: actionGroupFunction
-      },
-      actionGroupState: "ENABLED",
-      apiSchema: bedrock.ApiSchema.fromAsset(path.join(__dirname, 'action-group.yaml')),
-    });
-
-    agent.addActionGroups([actionGroup])
-
-    agent.addAlias({
-      aliasName: 'my-agent-alias',
-      description:'alias for my agent'
-      
-    })
+    //TODO
   
     new cdk.CfnOutput(this, 'AgentId', {value: agent.agentId});
     new cdk.CfnOutput(this, 'KnowledgeBaseId', {value: kb.knowledgeBaseId});
