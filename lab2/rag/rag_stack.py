f350ce51ef3be9e8fab7f5fb089d4a81dfe8e628
jmarcosttechsummit <jmarcost+techsummit@amazon.com>
2024-09-22 14:31:06 -0600

    Update rag_stack.py
    lab2 init

--- lab2/rag/rag_stack.py
+++ lab2/rag/rag_stack.py
@@ -19,67 +19,15 @@ class RAGStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        region = Stack.of(self).region
+
+        ##### Step 1: Configure S3 Buckets and upload files to S3
 
 
-        kb = bedrock.KnowledgeBase(self, 'DocKnowledgeBase', 
-            embeddings_model= bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,                  
-        )
+        ##### Step 2: Configure Bedrock S3 data Source and Knowledge base
 
-        documentBucket = s3.Bucket(self, 'DocumentBucket')
 
-        deployment = s3deploy.BucketDeployment(self, "DeployDocuments",
-            sources=[s3deploy.Source.asset("docs")],
-            destination_bucket=documentBucket
-        )
 
-        bedrock.S3DataSource(self, 'KBS3DataSource',
-            bucket= deployment.deployed_bucket,
-            knowledge_base=kb,
-            data_source_name='documents',
-            chunking_strategy= bedrock.ChunkingStrategy.FIXED_SIZE,
-            max_tokens=500,
-            overlap_percentage=20   
-        )
+        ##### Step 3: Create a Lambda function and API gateway to query the Knowledge base
 
-        kbQueryLambdaFunction = _lambda.Function(
-            self, 'KBQueryFunction',
-            runtime=_lambda.Runtime.PYTHON_3_12,
-            code=_lambda.Code.from_asset('lambda'),
-            handler='app.handler',
-            environment={
-                'KB_ID': kb.knowledge_base_id,
-                'KB_MODEL_ARN': 'arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0',
-            },
-            timeout=Duration.seconds(15)
-        )
 
-        kbArn = f'arn:aws:bedrock:{Stack.of(self).region}:{Stack.of(self).account}:knowledge-base/{kb.knowledge_base_id}'
-
-        policy_statement = iam.PolicyStatement(
-            actions=[
-                "bedrock:Retrieve",
-                "bedrock:RetrieveAndGenerate",
-                "bedrock:InvokeModel"
-            ],
-            resources=[
-                "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
-                kbArn
-            ]
-        )
-
-        kbQueryLambdaFunction.add_to_role_policy(policy_statement)
-
-        api = apigateway.LambdaRestApi(
-            self, 'KBQueryApiGW',
-            handler=kbQueryLambdaFunction,
-            proxy=False
-        )
-
-        kb_query = api.root.add_resource('query')
-        kb_query.add_method('POST')
-
-        CfnOutput(
-            self, 'ApiEndpoint',
-            value=api.url,
-            description='The endpoint of the KB Query API'
-        )
